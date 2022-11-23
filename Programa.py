d419f83b01d48ee0ad509518fec216fd704c53df
Santiago Suesca <dssuescav@correo.udistrital.edu.co>
2022-11-23 17:38:23 -0500

    0.0.2

---
+++ Programa.py
@@ -0,0 +1,12 @@
+from Punto import Punto
+
+messi=Punto()
+elbicho=Punto()
+
+messi.x=8
+messi.y=2
+
+elbicho.x=9
+elbicho.y=9
+
+a=messi.calcular_distancia(elbicho)
--- Punto.py
+++ Punto.py
@@ -2,12 +2,14 @@ import math
 
 class Punto:
     def __init__(self):
-        self.x=int
-        self.y=int
+        self.x=0
+        self.y=0
 
 
-def calcular_distancia(self):
-    self.calcular_distancia=math.hypot(self.x,self.y)
+def calcular_distancia(self, otroPunto):
+    resultado=math.sqrt(math.pow((self.x-otroPunto.x),2)+math.pow((self.y-otroPunto.y),2))
+
+    return resultado
 
 miPunto=Punto()
 otroPunto=Punto()
