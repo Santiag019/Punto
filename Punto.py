2930e9b414b76e53b60f47f566fe8fdbea7efa98
Santiago Suesca <dssuescav@correo.udistrital.edu.co>
2022-11-23 16:57:30 -0500

    0.1

---
+++ Punto.py
@@ -0,0 +1,16 @@
+import math
+
+class Punto:
+    def __init__(self):
+        self.x=int
+        self.y=int
+
+
+def calcular_distancia(self):
+    self.calcular_distancia=math.hypot(self.x,self.y)
+
+miPunto=Punto()
+otroPunto=Punto()
+
+
+
